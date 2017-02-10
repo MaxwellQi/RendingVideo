f88263c2dbef117a856c2e2ddbda2fc05b834c0c
QiZhang <qizhang@tvunetworks.com>
2017-02-10 14:17:17 +0800

    init project

---
+++ VideoDemo/VideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoDemo
+//
+//  Created by zhangqi on 10/2/2017.
+//  Copyright © 2017 MaxwellQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoDemo/VideoDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VideoDemo
+//
+//  Created by zhangqi on 10/2/2017.
+//  Copyright © 2017 MaxwellQi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
